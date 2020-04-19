5e07251e87c9e706079060f64981bd3c642ed64d
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2020-04-19 08:53:34 +0200

    Add files via upload

---
+++ precache-manifest.05c852d66f2c9ca2b05b666cab0958b2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451a5761cce511cd9e2f516a5beb1617",
+    "url": "/ShopReact/index.html"
+  },
+  {
+    "revision": "23963c13ea32c67f8057",
+    "url": "/ShopReact/static/css/main.7dae044f.chunk.css"
+  },
+  {
+    "revision": "12dc359a3d117b5c34c2",
+    "url": "/ShopReact/static/js/2.8c44a80e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ShopReact/static/js/2.8c44a80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23963c13ea32c67f8057",
+    "url": "/ShopReact/static/js/main.17de64a0.chunk.js"
+  },
+  {
+    "revision": "330313e32529d54a594a",
+    "url": "/ShopReact/static/js/runtime-main.b47141a5.js"
+  },
+  {
+    "revision": "23253faf14a25b1dd466f7a762e18d5c",
+    "url": "/ShopReact/static/media/accessories.23253faf.jpg"
+  },
+  {
+    "revision": "42ef78e0f53f4e59a8028399b415ed69",
+    "url": "/ShopReact/static/media/bag.42ef78e0.jpg"
+  },
+  {
+    "revision": "c92ff70b317c596a28b05b1b1f4ccde8",
+    "url": "/ShopReact/static/media/bag1.c92ff70b.jpg"
+  },
+  {
+    "revision": "4d48c0696919db63931bd95808ac1d9a",
+    "url": "/ShopReact/static/media/bag2.4d48c069.jpg"
+  },
+  {
+    "revision": "a719ffb01536cee9b3bee970e6fb6642",
+    "url": "/ShopReact/static/media/glasses1.a719ffb0.jpg"
+  },
+  {
+    "revision": "e5e98978f4a10895933025338705ced3",
+    "url": "/ShopReact/static/media/glasses11.e5e98978.jpg"
+  },
+  {
+    "revision": "5a888d99c8d7787d5d12de7b5e36b550",
+    "url": "/ShopReact/static/media/glasses3.5a888d99.jpg"
+  },
+  {
+    "revision": "a291861b0a6ffc4be3efe357dd2103ac",
+    "url": "/ShopReact/static/media/shoes1.a291861b.jpg"
+  },
+  {
+    "revision": "c1930efbeec1d89a679ed2a53a70109c",
+    "url": "/ShopReact/static/media/shoes3.c1930efb.jpg"
+  },
+  {
+    "revision": "b3fd33656410168fd9e6989d151d5de2",
+    "url": "/ShopReact/static/media/watch3.b3fd3365.jpg"
+  },
+  {
+    "revision": "86d6d691dd1178bff1c4e830c4c770f0",
+    "url": "/ShopReact/static/media/winter3.86d6d691.jpg"
+  },
+  {
+    "revision": "723dc5e25833abe8ffed7fe671d49915",
+    "url": "/ShopReact/static/media/winter6.723dc5e2.jpg"
+  }
+]);
